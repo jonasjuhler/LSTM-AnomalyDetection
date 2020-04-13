cc6824ff85d937ea8a8a6b7c2e02363a516b5af2
Jonas Juhler-Nøttrup <jonas.juhler.n@gmail.com>
2020-04-13 23:45:49 +0200

    The script now plots the final results

--- scripts/test_loss.py
+++ scripts/test_loss.py
@@ -27,7 +27,7 @@ print("Dimension of LSTM hidden state (encoder and decoder):", h_dim)
 print("Dimension of latent space variables z and c's:", z_dim)
 net = VRASAM(z_dim, T, x_dim, h_dim)
 criterion = ELBO_loss
-optimizer = torch.optim.Adam(net.parameters())
+optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
 
 fig, ax = plt.subplots(1, 2)
 ax[0].plot(X[0])
@@ -40,27 +40,63 @@ ax[1].set_ylim((0, 1))
 plt.ion()
 plt.show()
 
-for i in range(2000):
+
+for i in range(800):
     # Make forward pass with model
     # X = torch.Tensor(X).unsqueeze(1).unsqueeze(0)
     optimizer.zero_grad()
     outputs = net(X)
 
     mu_x = outputs['mu_x']
-    b_x = outputs['b_x']
 
     loss = criterion(X, outputs)
     loss.backward()
     optimizer.step()
 
     if i % 50 == 0:
-
-        x_gen = np.random.laplace(
-            loc=mu_x.detach().numpy(), scale=b_x.detach().numpy())
-
-        regen_line.set_ydata(x_gen[0])
-
+        regen_line.set_ydata(mu_x.detach().numpy())
         plt.draw()
         plt.pause(0.001)
-
         print(loss)
+
+plt.close()
+
+# Define normal and outlier type data
+outlier_type = "Snow"
+X_normal, _ = gendata(T=T)
+X_outlier, _ = gendata(T=T, outlier_type=outlier_type)
+X_normal = torch.Tensor(X_normal).unsqueeze(1).unsqueeze(0)
+X_outlier = torch.Tensor(X_outlier).unsqueeze(1).unsqueeze(0)
+
+# Create final regeneration
+outputs_normal = net(X_normal)
+outputs_outlier = net(X_outlier)
+mu_x_normal = outputs_normal['mu_x']
+mu_x_outlier = outputs_outlier['mu_x']
+
+
+fig, ax = plt.subplots(2, 2, figsize=(10, 7))
+
+# Plot ground truth
+ax[0, 0].plot(X_normal[0])
+ax[0, 0].set_ylim((0, 1))
+ax[0, 0].set_title('Ground truth')
+
+# Plot regenerated ground truth
+ax[0, 1].plot(mu_x_normal.detach().numpy()[0], 'r-')
+ax[0, 1].set_title('Regenerated ground truth')
+ax[0, 1].set_ylim((0, 1))
+
+# Plot outlier
+ax[1, 0].plot(X_outlier[0])
+ax[1, 0].set_ylim((0, 1))
+ax[1, 0].set_title('Outlier timeseries ({0})'.format(outlier_type.lower()))
+
+# Plot regenerated ground truth
+ax[1, 1].plot(mu_x_outlier.detach().numpy()[0], 'r-')
+ax[1, 1].set_title('Regenerated outlier data')
+ax[1, 1].set_ylim((0, 1))
+
+print("<Click figure to close>")
+plt.waitforbuttonpress(0)  # this will wait for indefinite time
+plt.close(fig)
