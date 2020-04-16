98902291f85f214988a5ef99bdd803c7ca1ec01d
Jonas Juhler-Nøttrup <jonas.juhler.n@gmail.com>
2020-04-17 01:36:21 +0200

    Modified script to do online mode

--- scripts/test_online_mode.py
+++ scripts/test_online_mode.py
@@ -5,29 +5,32 @@ from kongkat.dataload.generate_data import gendata, data_generator
 from kongkat.model.vrasam import VRASAM, ELBO_loss
 
 T = 96  # Length of sequences
-N = 800  # Number of sequences
+N = 100  # Number of sequences
 T_w = 32  # Window length
-N_w = int(T/T_w * N)  # Number of windows
+N_w = T - T_w + 1  # Number of windows for each sequence
 batch_size = 1
 x_dim = 1
+data_shape = (N, batch_size, T, x_dim)
+gen = data_generator(N, T=T)  # Generate N samples of length T
+X = torch.Tensor([X for X, _ in gen]).view(*data_shape)
 
-X = torch.Tensor([X for X, _ in data_generator(N, T=T)]
-                 ).view(N_w, batch_size, T_w, -1)
+# Model hyperparameters
 z_dim = 3
-h_dim = 4
-
+h_dim = 128  # Number of LSTM units in each direction
+lr = 0.001
+criterion = ELBO_loss
+net = VRASAM(z_dim, T_w, x_dim, h_dim)
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
 print("Initializing model with specs:")
 print("Batch size:", batch_size)
 print("Dimension of LSTM hidden state (encoder and decoder):", h_dim)
 print("Dimension of latent space variables z and c's:", z_dim)
-net = VRASAM(z_dim, T_w, x_dim, h_dim)
-criterion = ELBO_loss
-optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
 
 fig, ax = plt.subplots(1, 1)
 plot_lim = (-0.1, 1)
 
-true_line, = ax.plot(X[0, 0], 'b-', label='Ground truth')
+true_line, = ax.plot(range(T_w), np.random.rand(T_w),
+                     'b-', label='Normal data')
 ax.set_ylim(plot_lim)
 ax.set_title('Timeseries')
 regen_line, = ax.plot(range(T_w), np.random.rand(T_w),
@@ -38,25 +41,32 @@ plt.ion()
 plt.legend()
 plt.show()
 
+p = 0
 
-for i, x_i in enumerate(X):
-    # Make forward pass with model
-    optimizer.zero_grad()
-    outputs = net(x_i)
+for x_i in X:
+    # Sliding window
+    for w_start in range(N_w):
+        # Extract window from x_i
+        x = x_i[:, w_start:(w_start+T_w)]
 
-    mu_x = outputs['mu_x']
+        # Make forward pass with model
+        optimizer.zero_grad()
+        outputs = net(x)
 
-    loss = criterion(x_i, outputs)
-    loss.backward()
-    optimizer.step()
+        mu_x = outputs['mu_x']
 
-    if i % 50 == 0:
-        true_line.set_ydata(x_i.detach().numpy()[0])
-        regen_line.set_ydata(mu_x.detach().numpy())
-        plt.draw()
-        plt.pause(0.001)
-        print(loss)
+        loss = criterion(x, outputs, 0.01, 0.01)
+        loss.backward()
+        optimizer.step()
 
+        if p % 100 == 0:
+            true_line.set_ydata(x.detach().numpy()[0])
+            regen_line.set_ydata(mu_x.detach().numpy())
+            plt.draw()
+            plt.pause(0.001)
+            print(loss)
+
+        p += 1
 plt.close()
 
 # Define normal and outlier type data
